6068096457a9dadd6cd626b0ce7e9d37833f8d20
Kostas Georgiadis <kongeor@gmail.com>
2016-08-16 12:24:59 -0400

    Formatting fix

--- src/main/java/gr/lolo/config/DatabaseConfig.java
+++ src/main/java/gr/lolo/config/DatabaseConfig.java
@@ -15,6 +15,6 @@ public class DatabaseConfig {
     @Primary
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
-	return DataSourceBuilder.create().build();
+        return DataSourceBuilder.create().build();
     }
 }
