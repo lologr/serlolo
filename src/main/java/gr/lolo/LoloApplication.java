112256f53b408d98c28c04f7571a067473afb7b8
Kostas Georgiadis <kongeor@gmail.com>
2016-08-16 11:27:15 -0400

    Initial commit

---
+++ src/main/java/gr/lolo/LoloApplication.java
@@ -0,0 +1,12 @@
+package gr.lolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoloApplication.class, args);
+	}
+}
