7164f7711223c25248192f31161dfb9a01220d19
AndrewIatr <andreasiatr@gmail.com>
2016-08-20 14:59:29 +0300

    Implemented Slugifier

--- src/main/java/gr/lolo/util/Slugifier.java
+++ src/main/java/gr/lolo/util/Slugifier.java
@@ -1,20 +1,88 @@
 package gr.lolo.util;
 
+import com.google.common.collect.ImmutableMap;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriUtils;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.function.Supplier;
 
+import static java.util.stream.Collectors.joining;
+
+
 @Component
 public class Slugifier {
+    private static final ImmutableMap<Character, String> greekToLatinMap =  ImmutableMap.<Character, String>builder()
+            .put('α', "a")
+            .put('β', "b")
+            .put('γ', "g")
+            .put('δ', "d")
+            .put('ε', "e")
+            .put('ζ', "z")
+            .put('η', "h")
+            .put('θ', "th")
+            .put('ι', "i")
+            .put('κ', "k")
+            .put('λ', "l")
+            .put('μ', "m")
+            .put('ν', "n")
+            .put('ξ', "ks")
+            .put('ο', "o")
+            .put('π', "p")
+            .put('ρ', "r")
+            .put('σ', "s")
+            .put('ς', "s")
+            .put('τ', "t")
+            .put('υ', "u")
+            .put('φ', "f")
+            .put('χ', "x")
+            .put('ψ', "ps")
+            .put('ω', "w")
+            .build();
 
     public String slugify(String str) {
-        // TODO
-        return null;
+        String slugified = StringUtils.stripAccents(str)
+                .toLowerCase()
+                .replace(' ', '-')
+                .chars()
+                .mapToObj(i -> (char) i)
+                .map(greekChar -> greekToLatinMap.getOrDefault(greekChar, String.valueOf(greekChar)))
+                .collect(joining());
+        try {
+            return UriUtils.encode(slugified, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            // should never happen
+            throw new IllegalArgumentException(e);
+        }
     }
 
     public String slugify(String str, Supplier<List<String>> exclusionsSup) {
-        // TODO
-        return null;
+        String slugified = slugify(str);
+        List<String> exclusions = exclusionsSup.get();
+        if (exclusions.contains(slugified)) {
+            slugified += "-";
+            int idNum = getMinExcludedIdNum(exclusions);
+            while (exclusions.contains(slugified + ++idNum));
+            return slugified + idNum;
+        }
+        return slugified;
+    }
+
+    private int getMinExcludedIdNum(List<String> exclusions) {
+        return exclusions.stream()
+                .map(s -> {
+                    int dashIndx = s.lastIndexOf('-');
+                    if (dashIndx > -1) {
+                        String possibleNum = s.substring(++dashIndx);
+                        if (StringUtils.isNumeric(possibleNum)) {
+                            return Integer.parseInt(possibleNum);
+                        }
+                    }
+                    return 0;
+                })
+                .min(Integer::compareTo)
+                .orElse(0);
     }
 }
--- src/test/java/gr/lolo/util/SlugifierTest.java
+++ src/test/java/gr/lolo/util/SlugifierTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class SlugifierTest {
 
@@ -15,12 +15,28 @@ public class SlugifierTest {
         assertEquals("patata", slugifier.slugify("πατάτα"));
         assertEquals("patata", slugifier.slugify("πατατα"));
         assertEquals("patata", slugifier.slugify("Πατατα"));
+        assertEquals("keftedes", slugifier.slugify("ΚΕΦΤΕΔΕΣ"));
+        assertEquals("keftedes", slugifier.slugify("ΚΕΦΤΈΔΕΣ"));
+
+        assertEquals("elies-throumpes", slugifier.slugify("ελιές θρούμπες"));
+        assertEquals("ksifias", slugifier.slugify("ξιφίας"));
+
+        assertEquals("kounoupidi-me-arxidia", slugifier.slugify("Κουνουπίδι με αρχίδια"));
+        assertEquals("a-la-krem", slugifier.slugify("Α-λα κρεμ"));
+
+        assertEquals("m%27-aresoun-ta-paidakia", slugifier.slugify("μ' αρέσουν τα παϊδάκια"));
+
+        assertEquals("%26mpap", slugifier.slugify("&μπαπ"));
     }
 
     @Test
     public void slugify_with_exclusions_test() {
         assertEquals("patata-1", slugifier.slugify("πατάτα", () -> Arrays.asList("patata")));
-        assertEquals("patata-2", slugifier.slugify("πατάτα", () -> Arrays.asList("patata", "patata-2")));
+        assertEquals("patata", slugifier.slugify("πατάτα", () -> Arrays.asList("patata-0", "patata-1")));
+        assertEquals("patata-1", slugifier.slugify("πατάτα", () -> Arrays.asList("patata", "patata-2")));
+        assertEquals("patata-2", slugifier.slugify("πατάτα", () -> Arrays.asList("patata", "patata-1")));
+
+        assertEquals("elies-throumpes-2", slugifier.slugify("ελιές θρούμπες", () -> Arrays.asList("elies-throumpes", "elies-throumpes-1")));
     }
 
 }
